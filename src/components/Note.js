890a5612e665e21ab634a50fdd405a290aa18b9c
Hugo <hugogreg@live.fr>
2023-01-31 14:56:27 +0100

    can delete dude !

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,18 @@
 import { MdDeleteForever } from 'react-icons/md'
 import NotesList from './NotesList'
 
-const Note = ({id, text, date}) => {
+const Note = ({id, text, date, handleDeleteNote}) => {
   return (
     <div className="note">
 
       <span>{text}</span>
       <div className="note-footer">
         <small>{date}</small>
-        <MdDeleteForever className='delete-icon' size='1.3rem' />
+        <MdDeleteForever
+          className='delete-icon'
+          size='1.3rem'
+          onClick={()=> handleDeleteNote(id)}
+        />
       </div>
 
     </div>
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,11 +1,15 @@
 import Note from "./Note"
 import AddNote from "./AddNote"
 
-const NotesList = ({ notes, handleAddNote }) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
   return (
     <div className="notes-list">
       {notes.map((notes)=> (
-      <Note id={notes.id} text={notes.text} date={notes.date}/>
+        <Note id={notes.id}
+        text={notes.text}
+        date={notes.date}
+        handleDeleteNote={handleDeleteNote}
+        />
       ))}
       <AddNote handleAddNote={handleAddNote} />
     </div>
