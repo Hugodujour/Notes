593c49b03f38dc390ef3f2206621d5e2fb48d768
Hugo <hugogreg@live.fr>
2023-01-31 14:47:16 +0100

    notes fonctionnelles

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,12 +1,13 @@
 import Note from "./Note"
+import AddNote from "./AddNote"
 
-const NotesList = ({ notes }) => {
+const NotesList = ({ notes, handleAddNote }) => {
   return (
     <div className="notes-list">
       {notes.map((notes)=> (
       <Note id={notes.id} text={notes.text} date={notes.date}/>
       ))}
-      
+      <AddNote handleAddNote={handleAddNote} />
     </div>
   )
 }
